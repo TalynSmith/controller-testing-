2f646a1e37a218b124def160c47548b15f5e13b5
Talyn Smith <talynandrew@gmail.com>
2018-12-05 20:54:15 -0700

    needs more work

--- db/migrate/20181206020822_create_bank_accounts.rb
+++ db/migrate/20181206020822_create_bank_accounts.rb
@@ -2,10 +2,10 @@ class CreateBankAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :bank_accounts do |t|
       t.belongs_to :user, foreign_key: true
-      t.float :ammount
+      t.float :ammount, null:false, default: 0
       t.text :ledger
-      t.boolean :active
-      t.string :institution
+      t.boolean :active, null: false, default: true 
+      t.string :institution, null: false 
       t. :description
 
       t.timestamps
